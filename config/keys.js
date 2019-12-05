9fc01391ad9ea92d5278dccb28488a49ea7fdb85
samdevo <samdevorsetz@gmail.com>
2019-12-05 10:08:45 -0500

    client push

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,5 @@ module.exports = {
   mongoURI: "mongodb+srv://samdevo:cscapstone@cluster0-mgoxf.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "secret" 
 };
+
+//mongodb+srv://samdevo:<password>@cluster0-mgoxf.mongodb.net/test?retryWrites=true&w=majority
