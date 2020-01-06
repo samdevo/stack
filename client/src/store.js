40a67f9076e4d7a84dec3bcec328a6bdb21b5e2f
samdevo <samdevorsetz@gmail.com>
2020-01-06 15:01:38 -0500

    removed devtools bug

--- client/src/store.js
+++ client/src/store.js
@@ -8,8 +8,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    //ADD THIS FOR DEVTOOLS
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 // alert("2")
