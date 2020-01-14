89e2b3bb808b13b593afad633d967aac57891c2b
samdevo <samdevorsetz@gmail.com>
2020-01-14 14:24:48 -0500

    Update Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,8 +7,9 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames"
 
+import {getEvents} from "../../actions/eventActions";
 import pic from './elders.jpg';
-import "./home.css"
+import "./home.css";
 
 
 class Landing extends React.Component {
@@ -52,112 +53,7 @@ class Landing extends React.Component {
 			</div>
 
 		
-
-import {getEvents} from "../../actions/eventActions"
-
-class Landing extends React.Component {
-	constructor() {
-	    super();
-	    this.state = {
-	      name: "",
-	      email: "",
-	      password: "",
-	      password2: "",
-	      errors: {}
-	    };
-	    this.events = []
-	    this.eventListing = []
-	  }
-	  componentDidMount() {
-	    // If logged in and user navigates to Register page, should redirect them to dashboard
-	    // if (this.props.auth.isAuthenticated) {
-	    //   this.props.history.push("/dashboard");
-	    // }
-	    this.props.getEvents().then((events) => {
-	    	console.log(events)
-	    	this.events = events
-	    	
-	    })
-
-	  }
-	  
-
-	render() {
-		const { errors } = this.state
-		return(
-			<Container>
-			  <Row class='row' className="justify-content-md-center border">
-			    <Col class='border' xs lg="2">
-			      1 of 3
-			    </Col>
-			    <Col class='border' md="auto">Variable width content</Col>
-			    <Col class='border' xs lg="2">
-			      3 of 3
-			    </Col>
-			  </Row>
-			  <Row class='row' className="border">
-			    <Col class='border'>1 of 3</Col>
-			    <Col class='border' md="auto">Variable width content</Col>
-			    <Col class='border' xs lg="2">
-			      3 of 3
-			    </Col>
-			  </Row>
-
-		<Row>
-			<Col className="[ col-xs-12 col-sm-offset-2 col-sm-8 ]">
-				<ul class="event-list">
-					<li>
-						<time datetime="2014-07-20">
-							<span class="day">4</span>
-							<span class="month">Jul</span>
-							<span class="year">2014</span>
-							<span class="time">ALL DAY</span>
-						</time>
-						<img alt="Independence Day" src="https://farm4.staticflickr.com/3100/2693171833_3545fb852c_q.jpg" />
-						<div class="info">
-							<h2 class="title">Independence Day</h2>
-							<p class="desc">United States Holiday</p>
-						</div>
-					</li>
-
-					
-					
-				</ul>
-			</Col>
-		</Row>
-		<Row>
-			<Col>
-				<ul class="event-list">
-					<li>
-						<time datetime="2014-07-20">
-							<span class="day">4</span>
-							<span class="month">Jul</span>
-							<span class="year">2014</span>
-							<span class="time">ALL DAY</span>
-						</time>
-						<img alt="Independence Day" src="https://farm4.staticflickr.com/3100/2693171833_3545fb852c_q.jpg" />
-						<div class="info">
-							<h2 class="title">Independence Day</h2>
-							<p class="desc">United States Holiday</p>
-						</div>
-					</li>
-
-					
-					
-				</ul>
-			</Col>
-		</Row>
-
-			</Container>
-			)
-
-
-	}
-
-
-
-
-
+)}
 
 }
 	// class Register extends Component {
